e56710835a44d4e3d63bce91f4059ba11415a42a
Peter Yoo <trustintofu@gmail.com>
2022-12-31 22:43:25 -0800

    feat: Creates Header component.

---
+++ components/Header.components.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="py-10">
+        <h1 className="text-3xl flex justify-center font-Montserrat w-full py-2">
+          JavaScript Notes
+        </h1>
+        <div className="border-b border-[#141723] w-72 mx-auto"></div>
+      </div>
+    </>
+  );
+};
