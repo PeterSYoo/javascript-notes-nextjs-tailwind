0b5fa8fe8824a5ad67fdfd39a908dd26d0f71cdf
Peter Yoo <trustintofu@gmail.com>
2023-01-01 12:02:22 -0800

    refactor: Updates title.

--- components/Header.components.tsx
+++ components/Header.components.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
   return (
     <>
-      <div className="py-10">
-        <h1 className="text-3xl flex justify-center font-Montserrat w-full py-2">
-          JavaScript Notes
+      <div className="py-10 flex flex-col items-center justify-center">
+        <h1 className="text-3xl flex justify-center font-Montserrat w-full text-[#141723]">
+          Peter&apos;s JavaScript Notes
         </h1>
-        <div className="border-b border-[#141723] w-72 mx-auto"></div>
+        <div className="border-b border-[#a1a3ab] w-96 mx-auto my-2"></div>
       </div>
     </>
   );
