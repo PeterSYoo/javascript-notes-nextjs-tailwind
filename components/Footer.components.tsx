e0223ccffb687a7f68992c68613c45a6a9dece9a
Peter Yoo <trustintofu@gmail.com>
2023-01-02 20:07:14 -0800

    refactor: Changes to maintained.

--- components/Footer.components.tsx
+++ components/Footer.components.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
       <div className="pt-10 pb-5 flex flex-col items-center justify-center px-10">
         <div className="border-b border-[#797a7d] max-w-[950px] w-full mx-auto my-2"></div>
         <p className="text-xs text-[#59595c] flex justify-end max-w-[950px] w-full">
-          Created by Peter Yoo
+          Maintained by Peter Yoo
         </p>
       </div>
     </>
