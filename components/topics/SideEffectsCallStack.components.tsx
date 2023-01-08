48c194cda0d2244a3e565ff3f86fbd1ed9ae2368
Peter Yoo <trustintofu@gmail.com>
2023-01-08 15:10:35 -0800

    feat: Adds side effects call stack as a topic.

---
+++ components/topics/SideEffectsCallStack.components.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+import Markdown from 'markdown-to-jsx';
+import { Code } from '../Code.components';
+import { IoChevronForwardCircleSharp } from 'react-icons/io5';
+import { MdNotes } from 'react-icons/md';
+
+export const SideEffectsCallStack = () => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [markdown, setMarkdown] = useState<string>('');
+
+  useEffect(() => {
+    const fetchMarkdown = async () => {
+      try {
+        const res = await fetch(`/docs/sideEffectsCallStack.md`);
+        const markdownString = await res.text();
+        setMarkdown(markdownString);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMarkdown();
+  }, []);
+
+  return (
+    <>
+      {isClicked ? (
+        <>
+          <div
+            onClick={() => setIsClicked(!isClicked)}
+            className="isClicked-true"
+          >
+            <div className="flex items-center gap-5">
+              <MdNotes className="mb-[1.5px]" />
+              <span className="text-sm">
+                Is Anything That Isn&apos;t Directly Sent to the Call Stack, but
+                is Handled Elsewhere Considered a Side Effect?
+              </span>
+            </div>
+            <IoChevronForwardCircleSharp className="chevron-down" />
+          </div>
+        </>
+      ) : (
+        <div
+          onClick={() => setIsClicked(!isClicked)}
+          className="isClicked-false"
+        >
+          <div className="flex items-center gap-5">
+            <MdNotes className="mb-[1.5px]" />
+            <span className="text-sm">
+              Is Anything That Isn&apos;t Directly Sent to the Call Stack, but
+              is Handled Elsewhere Considered a Side Effect?
+            </span>
+          </div>
+          <IoChevronForwardCircleSharp className="chevron-right" />
+        </div>
+      )}
+      {isClicked ? (
+        <>
+          <div className="markdown-container">
+            <Markdown
+              options={{
+                overrides: {
+                  Code: {
+                    component: Code,
+                  },
+                },
+              }}
+              className="markdown"
+            >
+              {markdown}
+            </Markdown>
+            <div className="flex mt-10">
+              <button
+                onClick={() => setIsClicked(!isClicked)}
+                className="font-montserrat border py-3 rounded-lg border-[#787878] hover:bg-[#2b2b2b] hover:text-[#FFC58E] transition-all duration-100 ease-in-out text-sm hover:border-transparent w-full text-[#2b2b2b]"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
---
+++ public/docs/sideEffectsCallStack.md
@@ -0,0 +1,4 @@
+Yes, that is a good way to think about it. A side effect is any action that has an impact outside of the immediate execution context of a program. This can include interactions with external systems or resources, such as reading or writing to a database, making an HTTP request, interacting with the filesystem, or even modifying the DOM. These actions are considered side effects because they are not part of the program's call stack and are instead handled by external systems or resources.
+
+<br/>
+In general, it is often recommended to minimize side effects and to clearly identify and isolate them in order to make a program more predictable and easier to test. This is especially important in functional programming, where the focus is on pure functions that have no side effects and can be easily composed and combined to perform more complex tasks.
