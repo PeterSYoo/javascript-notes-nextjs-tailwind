72d222335bbc5985a4906d685a6e524058614e34
Peter Yoo <trustintofu@gmail.com>
2023-01-12 18:20:08 -0800

    refactor: Updates topics.

--- components/topics/markdown/SideEffectsMD.components.tsx
+++ components/topics/markdown/SideEffectsMD.components.tsx
@@ -71,6 +71,31 @@ export const SideEffectsMD = () => {
         purpose of the component. Examples of side effects include making an
         HTTP request, setting up an event listener.
       </p>
+      <h2 className="font-semibold">
+        Are Event Listeners considered side effects because it&apos;s calling a
+        browser API?
+      </h2>
+      <p>
+        Yes, event listeners are considered side effects because they involve an
+        interaction with an external system (in this case, the browser). When a
+        program sets an event listener, it is causing a callback function to be
+        registered with the browser, and the callback function will be executed
+        by the browser when the specified event occurs.
+      </p>
+      <p>
+        In general, any interaction with an external resource or system can be
+        considered a side effect. This can include reading or writing to a
+        database, making an HTTP request, interacting with the filesystem, or
+        even modifying the DOM. All of these actions have an impact outside of
+        the immediate execution context of the program and are therefore
+        considered side effects.
+      </p>
+      <p>
+        It is important to manage side effects carefully in order to ensure that
+        a program is predictable and easy to test. In some cases, it may be
+        necessary to isolate and clearly identify side effects in order to make
+        a program more manageable and maintainable.
+      </p>
     </>
   );
 };
