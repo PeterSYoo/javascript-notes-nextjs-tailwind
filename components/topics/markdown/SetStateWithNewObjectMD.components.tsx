3a3cee0f956c993f9e1d43086653ed945b426ed0
Peter Yoo <trustintofu@gmail.com>
2023-01-12 17:15:13 -0800

    feat: Populate set state with new object md.

--- components/topics/markdown/SetStateWithNewObjectMD.components.tsx
+++ components/topics/markdown/SetStateWithNewObjectMD.components.tsx
@@ -2,10 +2,34 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const SetStateWithNewObjectMD = () => {
-  const c1 = ``;
+  const c1 = `
+  import React, { useState } from 'react';
+
+  const MyFunctionalComponent = () => {
+    const [state, setState] = useState({
+      name: '',
+      age: 0
+    });
+
+    const handleChange = (event) => {
+      setState({
+        ...state,
+        [event.target.name]: event.target.value
+      });
+    }
+
+    return (
+      <div>
+        <input name="name" placeholder="Name" onChange={handleChange} />
+        <input name="age" placeholder="Age" onChange={handleChange} />
+      </div>
+    );
+  }
+  `;
 
   return (
     <>
+      <p className="italic font-serif text-[#2b2b2b]"></p>
       <SyntaxHighlighter
         language="javascript"
         style={darcula}
@@ -13,6 +37,13 @@ export const SetStateWithNewObjectMD = () => {
       >
         {c1}
       </SyntaxHighlighter>
+      <p>
+        In this example, the input fields have the `name` attribute set to
+        &quot;name&quot; and &quot;age&quot;, respectively. The `handleChange`
+        function is called when the user types in either input field, and it
+        uses the `event.target.name` and `event.target.value` properties to
+        update the state with the new values.
+      </p>
     </>
   );
 };
